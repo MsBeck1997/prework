43274c1dae6d0879a3435203e637fae0783e9ea1
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-03-13 19:39:30 -0600

    added starter index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>I completed the Prework</title>
+	</head>
+	<body>
+		<p>I completed the Prework!</p>
+	</body>
+</html>
